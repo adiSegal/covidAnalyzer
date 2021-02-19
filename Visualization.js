f090d802a82a36ac6791cfd72052d7fb9731b97f
adiSegal <adisegal1@gmail.com>
2021-02-19 20:05:34 +0000

    Update Visualization.js

--- Visualization.js
+++ Visualization.js
@@ -1,22 +1,17 @@
-const { plot } = require('nodeplotlib');
+import { plot } from "nodeplotlib";
 
-const plotDailyNewCasesByCountry = function(days, dailyNewCases){
+export const plotDailyNewCasesByCountry = function(days, dailyNewCases){
   const data = [{x: days, y: dailyNewCases, type: 'line'}];
   plot(data);
 };
 
-const plotTotalCasesPerCountry = function(locations, totalCases){
+export const plotTotalCasesPerCountry = function(locations, totalCases){
   const data = [{x: locations, y: totalCases, type: 'bar'}];
   plot(data);
 };
 
-const plotLifeExpectancy = function(lifeExpectancy, countries){
+export const plotLifeExpectancy = function(lifeExpectancy, countries){
   const data = [{x: countries, y: lifeExpectancy, type: 'bar'}];
   plot(data);
 };
 
-module.exports = {
-  plotDailyNewCasesByCountry,
-  plotTotalCasesPerCountry,
-  plotLifeExpectancy
-};
