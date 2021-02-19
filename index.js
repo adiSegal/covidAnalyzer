4bf72bd8713dc72288acde31176eb294dfefe9a0
adiSegal <adisegal1@gmail.com>
2021-02-19 20:17:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import { connectToDb, initDb, disconnectFromDb } from './Dao.js';
-import { insertCsvIntoDb, getCsvFromUrl } from './CsvHandler.js';
+import { initializeCovidDb } from './CovidAnalyzer.js';
 
 const CSV_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv";
 
 (async () => {
-  try {
-    const [csv] = await Promise.all([getCsvFromUrl(CSV_URL), initDb()]);
-    await connectToDb();
-    await insertCsvIntoDb(csv);
-    disconnectFromDb();
-    process.exit(0);
-  } catch (e) {
-    console.error("Error while initializing the app", e);
-    process.exit(1);
-  }
+  initializeCovidDb(CSV_URL);
 })();
